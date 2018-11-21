64c4ffb2458381deb07496388e9b2d16831d4ec8
Amanda <amandinha.g.2009@hotmail.com>
2018-11-21 20:41:36 -0300

    identando o codigo

--- CriptografiaAssimetrica/src/CriptografiaAssimetrica.java
+++ CriptografiaAssimetrica/src/CriptografiaAssimetrica.java
@@ -36,27 +36,27 @@ public class CriptografiaAssimetrica {
         	
         //DEFININDO O VALOR DE P
         int p_valor = 3;
-        System.out.println("Valor de P: "+p_valor);
+        System.out.println("Valor de P: " + p_valor);
         
         //DEFININDO O VALOR DE Q
         int q_valor = 11;
-        System.out.println("Valor de Q: "+q_valor);
+        System.out.println("Valor de Q: " + q_valor);
         
         //ACHANDO O VALOR DE N
         int n_valor = p_valor * q_valor;
-        System.out.println("Valor de N: "+ n_valor);
+        System.out.println("Valor de N: " + n_valor);
         
         // ACHANDO O VALOR DE Z
         int z_valor = (p_valor - 1)*(q_valor - 1);
-        System.out.println("Valor de Z: "+z_valor);
+        System.out.println("Valor de Z: " + z_valor);
         
         //ACHANDO O VALOR DE D
         int d_valor = 7;
-        System.out.println("Valor de D: "+d_valor);
+        System.out.println("Valor de D: " + d_valor);
         
         //ACHANDO O VALOR DE E
         int e_valor = 3;
-        System.out.println("Valor de E: "+e_valor);
+        System.out.println("Valor de E: " + e_valor);
          
 
 	}
