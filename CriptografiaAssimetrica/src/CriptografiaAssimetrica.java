1d2e4d9c3a8b60d5edd2490fcc516e84be8f78fd
Amanda <amandinha.g.2009@hotmail.com>
2018-11-21 20:38:19 -0300

    Criando o alfabeto e definindo os respectivos valores de cada letra

---
+++ CriptografiaAssimetrica/src/CriptografiaAssimetrica.java
@@ -0,0 +1,64 @@
+
+public class CriptografiaAssimetrica {
+
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//DEFININDO O VALOR DE CADA LETRA DO ALFABETO
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+        int f = 6;
+        int g = 7;
+        int h = 8;
+        int i = 9;
+        int j = 10;
+        int k = 11;
+        int l = 12;
+        int m = 13;
+        int n = 14;
+        int o = 15;
+        int p = 16;
+        int q = 17; 
+        int r = 18;
+        int s = 19;
+        int t = 20;
+        int u = 21;
+        int v = 22;
+        int w = 23;
+        int x = 24;
+        int y = 25;
+        int z = 26;
+        
+    
+        	
+        //DEFININDO O VALOR DE P
+        int p_valor = 3;
+        System.out.println("Valor de P: "+p_valor);
+        
+        //DEFININDO O VALOR DE Q
+        int q_valor = 11;
+        System.out.println("Valor de Q: "+q_valor);
+        
+        //ACHANDO O VALOR DE N
+        int n_valor = p_valor * q_valor;
+        System.out.println("Valor de N: "+ n_valor);
+        
+        // ACHANDO O VALOR DE Z
+        int z_valor = (p_valor - 1)*(q_valor - 1);
+        System.out.println("Valor de Z: "+z_valor);
+        
+        //ACHANDO O VALOR DE D
+        int d_valor = 7;
+        System.out.println("Valor de D: "+d_valor);
+        
+        //ACHANDO O VALOR DE E
+        int e_valor = 3;
+        System.out.println("Valor de E: "+e_valor);
+         
+
+	}
+
+}
